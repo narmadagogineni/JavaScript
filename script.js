dbfcf2b430eac8a117a525c0ad2d7a5e587ccb7d
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-08-22 19:33:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ let movies = [
     ["athadu", "nani", "dhookudu", "businessman"]
 ];
 
+for (let i=0; i<movies.length; i++) {
+    console.log(i, movies[i]);
+    for (let j=0; j<movies[i].length; j++) {
+        console.log(j, movies[i][j]);
+    }
+}
+
+
+
+--------------------------------------------------------------------------------------
+
+//backwards
+let movies = [
+    ["mirchi", "mr perfect", "darling"],
+    ["athadu", "nani", "dhookudu", "businessman"]
+];
+
 for (let i=movies.length-1; i>=0; i--) {
     console.log(i, movies[i]);
     for (let j=movies[i].length-1; j>=0; j--) {
