c0fde888ac4a65b164c638a48e870917e05d5141
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-08-23 10:50:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,18 @@ for (fruit of fruits) {
 }
 
 =-----------------------------------------------------------------------------------
+//SPLICE
 
+let arr = [1, 2, 3, 4, 5, 6, 2, 3];
+let num = 2;
+
+for(let i=0; i<arr.length; i++) {
+    if(num == arr[i]) {
+        arr.splice(i, 1);
+    }
+}
+
+console.log(arr);
 
 
 
